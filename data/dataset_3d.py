8016b587d2bedd9e2dbc93144a878be9b25cac7f
Neko <48724808+Mr-Neko@users.noreply.github.com>
2023-05-06 15:47:10 +0800

    Update dataset_3d.py

--- data/dataset_3d.py
+++ data/dataset_3d.py
@@ -3,7 +3,6 @@
  * All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- * By Le Xue
 '''
 
 import random
